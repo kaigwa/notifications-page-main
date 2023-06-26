54be186610291aa78446f0b58c124570972536d9
Reset <kaigwaedwin@gmail.com>
2023-06-26 10:18:32 +0300

    New Features

--- index.js
+++ index.js
@@ -1,13 +1,43 @@
-// call variables
+// create variables
 const btn = document.querySelector('button');
-const userName = document.querySelectorAll('.user');
 const noteNumber = document.querySelector('.number');
-const noteContent = document.querySelectorAll('.content');
-const redDot = document.getElementsByClassName('dot')
 
-// event listener with callBack function;
-btn.addEventListener('click', () =>{
-   noteContent.classList.remove('dot')
+const newNotify = document.querySelectorAll('.new');
+const li = document.querySelectorAll('.li');
+
+const noteContent = document.querySelectorAll('.content');
+const redDot = document.querySelectorAll('.dot');
+
+// create event listener and call forloop function, 
+// after button clicked;
+btn.addEventListener('click', (e) =>{
+    e.preventDefault();
+
+    for (let i = 0; i < newNotify.length; i++) {
+     newNotify[i].classList.remove('new');
+      
+     if (redDot[i].parentNode === noteContent[i]){
+        noteContent[i].removeChild(redDot[i]);
+        noteNumber.innerHTML = parseInt(noteNumber.innerHTML) - 1;
+     }
+    }
+});
+
+
+// onclick items create event with for loop function 
+// to toggle the items
+for (let i = 0; i < li.length; i++) {
+    li[i].addEventListener('click', (e) => {
+        e.preventDefault();
+
+        li[i].classList.remove('new');
+
+        if (redDot[i].parentNode === noteContent[i]) {
+            noteContent[i].removeChild(redDot[i]);
+            noteNumber.innerHTML = parseInt(noteNumber.innerHTML) - 1
+        }
+    });
     
-    // console.log(noteContent)
-})
+}
+
+// js-code end;
